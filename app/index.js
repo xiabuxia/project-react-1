b2f2f304ab5c5249f9880a7d9136f3ea5fef0548
夏锦胜 <814097909@qq.com>
2017-04-02 07:52:41 +0800

    引入jquery

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 
 import _ from 'lodash';
-
+import j from 'jquery';
 function component () {
-  var element = document.createElement('div');
-
+ // var element = document.createElement('div');
+  var element = j('<div></div>');
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  // element.innerHTML = _.join(['Hello','webpack'], ' ');
+    element.html(_.join(['Hello','webpack'], ' '))
+  return element.get(0);
 }
 
 document.body.appendChild(component());
