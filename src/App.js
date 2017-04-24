bca2deb84064812ccfb28c31ded99815ddda7d6f
夏锦胜 <814097909@qq.com>
2017-04-24 20:35:10 +0800

    消除不要直接修改state的警告

--- src/App.js
+++ src/App.js
@@ -50,8 +50,10 @@
       )
     }
     onSignUp(user){
-      this.state.user = user
-      this.setState(this.state)
+
+      let stateCopy = JSON.parse(JSON.stringify(this.state))
+      stateCopy.user = user
+      this.setState(stateCopy)
     }
       componentDidUpdate (){
 
