a33ac3cfa1856b8b233edb5690fa02da77ababec
夏锦胜 <814097909@qq.com>
2017-04-24 21:13:42 +0800

    合并 onSignUp 和 onSignIn

--- src/App.js
+++ src/App.js
@@ -52,8 +52,8 @@
          {this.state.user.id ? 
          null : 
          <UserDialog 
-            onSignUp={this.onSignUp.bind(this)}
-            onSignIn={this.onSignIn.bind(this)}/>}
+            onSignUp={this.onSignUpOrSignIn.bind(this)} 
+            onSignIn={this.onSignUpOrSignIn.bind(this)}/>}/>}
         </div>
       )
     }
@@ -63,13 +63,8 @@
       stateCopy.user = {}
       this.setState(stateCopy)
     }
-    onSignUp(user){
 
-      let stateCopy = JSON.parse(JSON.stringify(this.state))
-      stateCopy.user = user
-      this.setState(stateCopy)
-    }
-    onSignIn(user){
+    onSignUpOrSignIn(user){
       let stateCopy = JSON.parse(JSON.stringify(this.state)) 
       stateCopy.user = user
       this.setState(stateCopy)
