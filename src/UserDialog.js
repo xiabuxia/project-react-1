5f27cec4c04c4570adf79bfbc023a71242af862d
夏锦胜 <814097909@qq.com>
2017-04-24 19:42:54 +0800

    将 changeUserName 和 changePassword 优化成一个函数 changeFormData

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -19,18 +19,24 @@ export default class UserDialog extends Component {
     signUp(e){}
     signIn(e){}
 
+
+    changeFormData(key,e){
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.formData[key] = e.target.value
+        this.setState(stateCopy)
+    }
     render(){
         let signUpForm = (
             <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
             <div className="row">
                 <label>用户名</label> 
                 <input type="text"  value={this.state.formData.username}
-                        onChange={this.changeUsername.bind(this)}/>
+                        onChange={this.changeFormData.bind(this, 'username')}/>
             </div>
             <div className="row">
                 <label>密码</label>
                 <input type="password" value={this.state.formData.password}
-                       onChange={this.changePassword.bind(this)}/>
+                      onChange={this.changeFormData.bind(this, 'password')}/>
             </div>
             <div className="row actions">
                 <button type="submit">注册</button>
@@ -42,12 +48,12 @@ export default class UserDialog extends Component {
                 <div className="row">
                     <label>用户名</label>
                     <input type="text" value={this.state.formData.username}
-                            onChange={this.changeUsername.bind(this)}/>
+                            onChange={this.changeFormData.bind(this, 'username')}/>
                 </div>
                 <div className="row">
                     <label>密码</label>
                     <input type="password" value={this.state.formData.password}
-                           onChange={this.changePassword.bind(this)}/>
+                           onChange={this.changeFormData.bind(this, 'password')}/>
                 </div>
                 <div className="row actions">
                      <button type="submit">登录</button>
